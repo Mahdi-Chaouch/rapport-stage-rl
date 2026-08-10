a5d8bae1987c7ed26dd2e7671bc5d9c15799a8c0
constanlc <t2z7k6vzkf@privaterelay.appleid.com>
2026-08-10 10:33:55 +0200

    Link ISG logo to ISG Luxury website
    Wrap ISG logo in a link to the ISG Luxury website.

--- app/page.tsx
+++ app/page.tsx
@@ -170,11 +170,13 @@ export default function Home() {
                   <span>·</span>
                   <span className="text-neutral-300">Tuteur de stage : {global.companyTutor}</span>
                 </div>
-                <img
-                  src="/isg-logo.jpg"
-                  alt="ISG Luxury Program"
-                  className="h-16 w-auto opacity-30 invert"
-                />
+                <a href="https://isg-luxury.fr" target="_blank" rel="noopener noreferrer">
+  <img
+    src="/isg-logo.jpg"
+    alt="ISG Luxury Program"
+    className="h-16 w-auto opacity-30 invert"
+  />
+</a>
               </div>
             </section>
 
