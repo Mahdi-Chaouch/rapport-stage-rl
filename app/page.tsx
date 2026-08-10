e63d416d38ba1697708a053546050abe5245d4df
constanlc <t2z7k6vzkf@privaterelay.appleid.com>
2026-08-10 10:43:01 +0200

    Enhance logo opacity on hover for better visibility

--- app/page.tsx
+++ app/page.tsx
@@ -174,7 +174,7 @@ export default function Home() {
   <img
     src="/isg-logo.jpg"
     alt="ISG Luxury Program"
-    className="h-16 w-auto opacity-30 invert"
+    className="h-16 w-auto opacity-30 hover:opacity-60 transition-opacity duration-300 invert"
   />
 </a>
               </div>
